1c8b02025593480f65bd0d50728d24299b8b4949
bag9l <vadimlyashok03@gmail.com>
2023-01-20 18:16:53 +0200

    create repositories

---
+++ src/main/java/com/botscrew/universitymanager/repository/LectorRepository.java
@@ -0,0 +1,7 @@
+package com.botscrew.universitymanager.repository;
+
+import com.botscrew.universitymanager.model.Lector;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LectorRepository extends JpaRepository<Lector, String> {
+}
