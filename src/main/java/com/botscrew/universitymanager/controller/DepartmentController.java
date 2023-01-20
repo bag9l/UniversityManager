1f2de78e95dda328b4ee70cce79efb4e47728feb
bag9l <vadimlyashok03@gmail.com>
2023-01-20 20:03:16 +0200

    implement HTTP methods for CRUD operations

--- src/main/java/com/botscrew/universitymanager/controller/DepartmentController.java
+++ src/main/java/com/botscrew/universitymanager/controller/DepartmentController.java
@@ -1,9 +1,14 @@
 package com.botscrew.universitymanager.controller;
 
+import com.botscrew.universitymanager.dto.DepartmentDTO;
+import com.botscrew.universitymanager.model.Department;
 import com.botscrew.universitymanager.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/departments")
@@ -14,5 +19,36 @@ public class DepartmentController {
     @Autowired
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Department>> getDepartments(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                departmentService.getAllDepartments());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable("id") String id){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                departmentService.getDepartmentById(id));
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Department> createDepartment(@RequestBody DepartmentDTO dto){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                departmentService.addDepartment(dto));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Department> updateDepartment(@RequestBody DepartmentDTO dto,
+                                                       @PathVariable("id") String id){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                departmentService.updateDepartment(dto,id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteDepartment(@PathVariable("id") String id){
+        departmentService.deleteDepartmentById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/main/java/com/botscrew/universitymanager/controller/LectorController.java
+++ src/main/java/com/botscrew/universitymanager/controller/LectorController.java
@@ -1,9 +1,14 @@
 package com.botscrew.universitymanager.controller;
 
+import com.botscrew.universitymanager.dto.LectorDTO;
+import com.botscrew.universitymanager.model.Lector;
 import com.botscrew.universitymanager.service.LectorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/lectors")
@@ -14,5 +19,36 @@ public class LectorController {
     @Autowired
     public LectorController(LectorService lectorService) {
         this.lectorService = lectorService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Lector>> getLectors() {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                lectorService.getAllLectors());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Lector> getLectorById(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                lectorService.getLectorById(id));
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Lector> createLector(@RequestBody LectorDTO dto) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                lectorService.addLector(dto));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Lector> updateLector(@RequestBody LectorDTO dto,
+                                               @PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                lectorService.updateLector(dto, id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteLector(@PathVariable("id") String id){
+        lectorService.deleteLectorById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
