345603969a7e4897ad3aeeef6f6353d07755d373
bag9l <vadimlyashok03@gmail.com>
2023-02-05 19:06:24 +0200

    Implement a better way to search lectors using LIKE operator

--- src/main/java/com/botscrew/universitymanager/repository/LectorRepository.java
+++ src/main/java/com/botscrew/universitymanager/repository/LectorRepository.java
@@ -1,14 +1,20 @@
 package com.botscrew.universitymanager.repository;
 
 import com.botscrew.universitymanager.model.Lector;
+import com.botscrew.universitymanager.repository.custom.CustomizedLectorRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface LectorRepository extends JpaRepository<Lector, String> {
+public interface LectorRepository extends JpaRepository<Lector, String>, CustomizedLectorRepository {
 
-    @Query("SELECT l FROM Lector l WHERE LOWER(l.firstname) LIKE LOWER(CONCAT('%', :template,'%')) OR LOWER(l.lastname) LIKE LOWER(CONCAT('%', :template,'%'))")
+    @Query("""
+            SELECT l FROM Lector l 
+            WHERE 
+                LOWER(CONCAT(l.firstname,\" \", l.lastname)) 
+                LIKE LOWER(CONCAT('%', :template,'%'))
+            """)
     List<Lector> findLectorsContainsTemplate(@Param("template") String template);
 }
